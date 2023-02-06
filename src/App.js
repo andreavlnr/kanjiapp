7836f60bfea410024a559ab64c3e455144babe4f
Andrea Velner <andrea.velner.50@gmail.com>
2023-02-06 01:51:45 +0100

    remove errors and play with map

--- src/App.js
+++ src/App.js
@@ -3,46 +3,83 @@ import './App.css';
 
 function App() {
 
+  function GenerateKanjis(){
   const [data, setData] = useState([]);
   const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '399150df6cmsh35f5184d12a3c5dp112fc8jsn0d41cd98cdc1',
-    'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '399150df6cmsh35f5184d12a3c5dp112fc8jsn0d41cd98cdc1',
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
   }
-};
+  useEffect(() => {
 
-fetch('https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?on=シン', options)
-  .then(response => response.json())
-  .then(json => {
-    console.log("json", json)
-    setData(json)
-  }).catch(err => console.error(err));
-
-  /*const url = "https://kanjialive-api.p.rapidapi.com/api/public/kanji/訪";
-
-    fetch(url)
+  fetch('https://kanjialive-api.p.rapidapi.com/api/public/kanji/all', options)
     .then(response => response.json())
     .then(json => {
       console.log("json", json)
       setData(json)
-    }).catch(e=>{
-      console.log("e", e)
-    })*/
+    }).catch(err => console.error(err));
 
-  return (
-    <div className="App">
-      <h2>welcome</h2>
-      {
-        data.map(item => {
-          return (
-          <div>{item}</div>
-          )
-        })
-      }
-    </div>
-    );
+    }, [])
+
+    data?map(item => {
+      return {item}
+    })
   
-}
+
+
+};
+
+  
+
+    //kideployolni github pagesre
+    //travisnek van doksija erre
+    //PAL travis configját megnézni
+    //localStoragebe / Reduxba menteni lekért adatokat (?) és akkor így cacheből fogja betölteni
+
+  function GenerateData(){
+    const [items,setItems]=useState([]) 
+
+        const options = {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': '399150df6cmsh35f5184d12a3c5dp112fc8jsn0d41cd98cdc1',
+            'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+          }
+        };
+  
+    const url='https://kanjialive-api.p.rapidapi.com/api/public/kanji/all';
+
+    useEffect(() => {
+
+      fetch(url, options)
+        .then(res => res.json())
+        .then(data => {
+            setItems(data)
+          },
+        )
+     }, [])
+    return items; 
+  }
+
+
+  function MapThroughAllKanji(data){
+    data?.map(item => {
+          return {item}
+        })
+    return "no data"
+  }
+
+
+    return (
+      <div className="App">
+        <h2>welcome</h2>
+        { GenerateKanjis()}
+      </div>
+  )
+  }
+
+ 
 
 export default App;
